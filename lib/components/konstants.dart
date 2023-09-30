e4baef52923bed9a962cb5fd265f9dfcc536dbf2
Lavanya <lavanya@Lavanyas-MacBook-Air.local>
2023-10-01 01:42:03 +0530

    First commit

---
+++ lib/components/konstants.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+const kbarheight = 80.0;
+var kbarcolour = 0xFFEE869F;
+
+const kactivecolour = Color(0xFF707070);
+const kinactivecolour = Color(0xFF999999);
+
+const kdesign = TextStyle(
+fontSize: 20.0,
+color: Colors.black,
+);
+const kwhiteabold =TextStyle(
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kpinkabold =TextStyle(
+  color: Color(0xFFEE869F),
+  fontSize: 40.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kwhiteaboldlarge =TextStyle(
+  fontSize: 60.0,
+  fontWeight: FontWeight.w900,
+);
---
+++ lib/screens/input_page.dart
@@ -0,0 +1,264 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'reusable.dart';
+import 'IconContent.dart';
+import 'konstants.dart';
+import 'results.dart';
+import 'bottomButtom.dart';
+import 'package:bmi_calculator/Calculations.dart';
+
+enum Gender {
+  female,
+  male,
+}
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  Gender? gender;
+  int height = 20;
+  int weight = 20;
+  int age = 20;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('BMI CALCULATOR'),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Expanded(
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: reusable(
+                    OnPress: () {
+                      setState(() {
+                        gender = Gender.male;
+                      });
+                    },
+                    colour: gender == Gender.male ? kactivecolour : kinactivecolour,
+                    cardchild: IconContent(icon: FontAwesomeIcons.mars, content: 'Male'),
+                  ),
+                ),
+                Expanded(
+                  child: reusable(
+                    OnPress:() {
+                      setState(() {
+                        gender = Gender.female;
+                      });
+                    },
+                    colour: gender == Gender.female ? kactivecolour : kinactivecolour,
+                    cardchild: IconContent(icon: FontAwesomeIcons.venus, content: 'Female'),
+                  ),
+                ),
+              ],
+            ),
+          ),//male and female
+          Expanded(
+            child: reusable(
+              colour: kactivecolour,
+              cardchild: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('Height',
+                    style: kdesign,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(
+                        height.toString(),
+                        style: kwhiteabold,
+                      ),// Height in number
+                      Text(
+                        'cm',
+                        style: kdesign,
+                      )//cm
+                    ],
+                  ),
+                  SliderTheme(
+                      data: SliderTheme.of(context).copyWith(
+                        thumbColor: Color(kbarcolour),
+
+                        thumbShape:RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                        overlayShape: RoundSliderOverlayShape(overlayRadius: 35.0),
+                        overlayColor: Color(0x1fEE869F),
+                      ),
+                      child: Slider(
+                      value: height.toDouble(),
+                      min: 20.0,
+                      max:220.0,
+                      activeColor: Color(kbarcolour),
+                      inactiveColor: Colors.grey,
+                      onChanged: (double newValue){
+                        setState((){
+                          height = newValue.round();
+                        });
+                      }
+                  )
+          )
+                ]
+              ),
+            ),
+          ),//height with slider
+          Expanded(
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+
+                Expanded(
+                  child: reusable(
+                      colour: kactivecolour,
+                    cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Weight',
+                        ),//height title
+                        SizedBox(
+                          width: 12.0,
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: kwhiteabold,
+                        ),//select height
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            CustomButton(
+                                icon: FontAwesomeIcons.plus,
+                              onPressed:(){
+                                  setState(() {
+                                    weight++;
+                                  });
+                              }
+                            ),
+                            SizedBox(
+                              width: 20.0,
+                            ),
+                            CustomButton(icon: FontAwesomeIcons.minus,
+                                onPressed:(){
+                                  setState(() {
+                                    weight--;
+                                  });
+                                }
+                                ),
+                          ]
+                        )
+
+                      ],
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: reusable(
+                    colour: kactivecolour,
+                    cardchild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'Age',
+                        ),//height title
+                        SizedBox(
+                          height: 12.0,
+                        ),
+                        Text(
+                          age.toString(),
+                          style: kwhiteabold,
+                        ),//select height
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              CustomButton(
+                                  icon: FontAwesomeIcons.plus,
+                                  onPressed:(){
+                                    setState(() {
+                                      age++;
+                                    });
+                                  }
+                              ),
+                              SizedBox(
+                                width: 20.0,
+                              ),
+                              CustomButton(icon: FontAwesomeIcons.minus,
+                                  onPressed:(){
+                                    setState(() {
+                                      age--;
+                                    });
+                                  }
+                              ),
+                            ]
+                        )
+
+                      ],
+                    ),
+
+                  ),
+                ),
+              ],
+            ),
+          ),//weight nd age with buttons
+          BottomButton(
+    title: 'CALCULATE',
+    onTap: (){
+      Calculations calc =
+      Calculations(height: height, weight: weight);
+
+      Navigator.push(context, MaterialPageRoute(builder:
+    (context) => Results(
+      bmiResult: calc.calculateBMI(),
+      resultText: calc.getResult(),)));
+    },
+          ),
+
+        ],
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+
+  CustomButton({required this.icon, required this.onPressed});
+  final IconData icon;
+  final Function onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+        onPressed:() {},
+        //   setState((){
+        //     if(icon == FontAwesomeIcons.plus){
+        //       weight++;
+        //     }
+        //     else{
+        //       weight--;
+        //     }
+        //   });
+        // },
+
+        constraints: BoxConstraints.tightFor(
+          width: 56.0,
+          height: 56.0,
+        ),
+        child: Icon(
+          icon,
+        ),
+
+        shape: BeveledRectangleBorder(),
+        fillColor: kinactivecolour,
+        elevation: 15.0,
+
+      );
+
+    }
+
+}
+
